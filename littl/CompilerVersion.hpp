671fe02bf469749c7bdac305b612bf2bb97e48f6
Xeatheran Minexew <minexew@gmail.com>
2016-11-12 18:34:32 +0100

    Add compiler detection for Visual Studio 2015

--- littl/CompilerVersion.hpp
+++ littl/CompilerVersion.hpp
@@ -44,7 +44,9 @@
 #define li_compiler_name        "Microsoft Visual C++"
 
 // Kinda pointless as we only support 11.0+
-#if _MSC_VER == 1800
+#if _MSC_VER == 1900
+#define li_compiler_version     "14.0 (Visual Studio 2015)"
+#elif _MSC_VER == 1800
 #define li_compiler_version     "12.0 (Visual Studio 2013)"
 #elif _MSC_VER == 1700
 #define li_compiler_version     "11.0 (Visual Studio 2012)"
